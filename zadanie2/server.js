90e3f1e37a0d22eae651a4b60f935b2703284011
215175 <215175@CARBON.CTI.P.LODZ.PL>
2018-03-25 11:47:36 +0200

    Add validator to node

--- zadanie2/server.js
+++ zadanie2/server.js
@@ -3,6 +3,8 @@ var app = express();
 var reverse = require('reverse-string');
 var port = process.env.PORT || 8088;
 
+//https://github.com/chriso/validator.js - man
+
 app.get('/:text', function(req, res){
 	res.send(reverse(req.params.text))}
 );
