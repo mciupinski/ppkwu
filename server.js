b83507b7d9074cf39b208abfe77e2884b79a65dc
215175 <215175@CARBON.CTI.P.LODZ.PL>
2018-03-25 11:19:20 +0200

    Reversed input

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var reverse = require('reverse-string');
 var port = process.env.PORT || 8088;
 
 app.get('/:text', function(req, res){
-	res.send(request.params.text.split("").reverse().join(""))}
+	res.send(reverse(req.params.text))}
 );
 app.listen(port, () => console.log('Example app listening on port 8088!'))
