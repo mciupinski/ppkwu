45f16320754a42178edbb1fe0f656ad9099baf5f
215175 <215175@CARBON.CTI.P.LODZ.PL>
2018-03-25 11:13:33 +0200

    npm reverse

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8088;
 
-
-app.get('/:rev', function(req, res){
-	res.send(request.params.rev.split("").reverse().join(""));
-})
-	app.listen(port, () => console.log('Example app listening on port 8088!'))
+app.get('/:text', function(req, res){
+	res.send(request.params.text.split("").reverse().join(""))}
+);
+app.listen(port, () => console.log('Example app listening on port 8088!'))
