b80bb470dbeec7cdcb993db584a9efa9ada759f0
unknown <Student@L406-18.carbon.cti.p.lodz.pl>
2018-04-08 10:43:17 +0200

    HTML Changes

--- zadanie2/server.js
+++ zadanie2/server.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
-var reverse = require('reverse-string');
+var validator = require('validator')
 var port = process.env.PORT || 8088;
 
 //https://github.com/chriso/validator.js - man
+app.use(require('body-parser').json());
 
 app.get('/:text', function(req, res){
 	res.send(reverse(req.params.text))}
 );
+app.get('/', (request, response) => response.sendFile(path.join(__dirname+'\\index.html')));
+app.post('/:type/:value*?', function(req, res){
+	let name = req.body.name;
+	let mail = req.body.mail;
+	if (true){
+		res.send("ok");
+	}
+	}
+}
+);
+
 app.listen(port, () => console.log('Example app listening on port 8088!'))
